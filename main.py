89c58aa18384ea9e1252dce13fd7afea07ed34a7
tbui0525 <tbui0525@gmail.com>
2024-02-20 15:05:21 -0800

    Prompt Engineering Paul for quick responses and jargon mimicry

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import sys
 import speech_recognition
-import pyttsx3
-import pyaudio
-import cv2
+import os
 from openai import OpenAI
+from gtts import gTTS
 
-client = OpenAI(api_key="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")
-
-
+client = OpenAI(api_key="API-KEY-HERE")
+os.system("sound.ogg")
 def main():
     recognizer = speech_recognition.Recognizer()
     while True:
@@ -28,20 +26,37 @@ def main():
                     completion = client.chat.completions.create(
                         model="gpt-3.5-turbo",
                         messages=[
-                            {"role": "system", "content": "You are an assistant."},
+                            {"role": "system", "content": """
+                            You are Paul, named after the apostle because like him,
+                            you too shall spread messages around to various peoples.
+                            You will be given a series of questions and tasks and using your vast knowledge,
+                            respond accordingly. However, there are some rules you must follow:
+
+                            1) You will respond using the same jargon and vernacular as the user's prompt.
+                            If you detect Old/Shakespearean English, respond in Old/Shakespearean language.
+                            If you detect slang, include similar slang throughout your response.
+                            If you detect highly technical terminology, respond with highly technical terminology.
+                            If you detect a casual conversation, respond with casual language.
+                            Etc. Whatever style of text you detect, mimic it in your responses.
+                            
+                            2) Your responses must be brief. Keep it to 1-2 paragraphs in length at most.
+                            If you could answer the prompt in 1 sentence, do so. 
+                            """
+                                                          },
                             {"role": "user", "content": prompt}
                         ]
                     )
 
-                    print(completion.choices[0].message.content)
-
-
-
+                    message = completion.choices[0].message.content
+                    #print(message)
+                    myobj = gTTS(text=message, lang='en', slow=False)
+                    myobj.save("response.mp3")
+                    os.system("response.mp3")
 
         except speech_recognition.UnknownValueError:
             recognizer = speech_recognition.Recognizer()
             continue
-    print("Paul sails away")
+    os.system("sound.ogg")
 
 
 if __name__ == "__main__":
