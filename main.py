dd67e09d1c951236975c331d488db5db9813a436
tbui0525 <tbui0525@gmail.com>
2024-03-06 07:51:04 -0800

    Added Spotify

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
-import os
 import json
+import os
 import sys
 
 import speech_recognition
 from gtts import gTTS
 from openai import OpenAI
+from spotify import spotify_search as sps
+
 
 # GETTING THE API KEY
-with open('OPEN_AI_API_KEY.txt', 'r') as f:
-    OPEN_AI_API_KEY = f.readline()
-f.close()
+with open("SECRETS.json", "r") as f:  # JSON
+    pw = json.load(f)[0]
+    f.close()
+OPEN_AI_API_KEY = pw["OPEN_AI_API_KEY"]
+
 # SETTING UP CLIENT
 client = OpenAI(api_key=OPEN_AI_API_KEY)
 # RETRIEVING MESSAGE HISTORY TO MAKE PAUL CARRY CONTEXT AFTER BEING TURNED ON AND OFF
-with open('history.json', 'r') as g:  # JSON
+with open("history.json", "r") as g:  # JSON
     history = json.load(g)
     g.close()
 print(len(history))
 # INITIALIZE MESSAGES TO ESTABLISH PAUL'S CHARACTER
 if len(history) == 0:
-    messages = [{
-        "role": "system",
-        "content": """
+    messages = [
+        {
+            "role": "system",
+            "content": """
                 You are Paul, named after the apostle because like him,
                 you too shall spread messages around to various peoples.
                 You will be given a series of questions and tasks and using your vast knowledge,
@@ -43,8 +48,9 @@ if len(history) == 0:
                 If the user's jargon has shifted, you will shift accordingly to match their vernacular.
                 Nonetheless, the most important part is the content and if sacrificing the jargon mimicry is 
                 required to give a sufficient answer to the prompt, do so.
-                """
-    }]
+                """,
+        }
+    ]
 else:
     messages = history
 
@@ -54,13 +60,17 @@ os.system("sound.ogg")
 
 def main():
     recognizer = speech_recognition.Recognizer()
+
     while True:
 
         try:
             with speech_recognition.Microphone() as mic:
                 recognizer.adjust_for_ambient_noise(mic, duration=0.2)
                 audio = recognizer.listen(mic)  # PICKS UP AUDIO SOUNDS
-                text = recognizer.recognize_google(audio)  # CONVERTS TO TEXT FOR FUTURE USE
+                # CONVERTS TO TEXT FOR FUTURE USE
+                text = recognizer.recognize_google(
+                    audio
+                )
 
                 words = text.split(" ")
                 word_count = len(text.split())
@@ -70,27 +80,39 @@ def main():
                 This prevents GPT from answering questions when it shouldn't and reduces confusion.
                 """
                 if word_count < 3:
-                    if words[0] in ["exit", "quit", "stop", "bye"]:  # COULD BE MORE IF WANTED
+                    if words[0] in [
+                        "exit",
+                        "quit",
+                        "stop",
+                        "bye",
+                    ]:  # COULD BE MORE IF WANTED
                         break
                 elif words[1] == "Paul":
-                    prompt = text.split("Paul ", 1)[1]  # ONLY SUBMITS WORDS AFTER 'PAUL' TO ANSWER PROMPT
+                    if words[2] == "play":
+                        prompt = text.split("play",1)[1]
+                        sps(prompt)
+                    else:
+                        prompt = text.split("Paul ", 1)[
+                        1
+                        ]  # ONLY SUBMITS WORDS AFTER 'PAUL' TO ANSWER PROMPT
 
-                    print(prompt)
-                    messages.append({"role": "user", "content": prompt})
-                    completion = client.chat.completions.create(
-                        model="gpt-3.5-turbo",
-                        messages=messages
-
-                    )
+                    # LANGUAGE DETECTION
+                    # detected_lang = language_detector.detect_language(prompt)
+                    # if detected_lang != 'en':
+                    #     prompt = translator.translate(promp, src=detected_lang, dest='en')
+                        messages.append({"role": "user", "content": prompt})
+                        completion = client.chat.completions.create(
+                            model="gpt-3.5-turbo", messages=messages
+                        )
                     # MESSAGE TO BE INTERPRETED
-                    message = completion.choices[0].message.content
+                        message = completion.choices[0].message.content
                     # CONVERTS TO AUDIO
-                    audio_response = gTTS(text=message, lang="en", slow=False)
-                    audio_response.save("response.mp3")
+                        audio_response = gTTS(text=message, lang="en", slow=False)
+                        audio_response.save("response.mp3")
                     # PLAYS AUDIO
-                    os.system("response.mp3")
+                        os.system("response.mp3")
                     # RETAINS CONTEXT
-                    messages.append({"role": "assistant", "content": message})
+                        messages.append({"role": "assistant", "content": message})
 
         except speech_recognition.UnknownValueError:
             recognizer = speech_recognition.Recognizer()
---
+++ spotify.py
@@ -0,0 +1,25 @@
+import spotipy
+import webbrowser
+import json
+with open("SECRETS.json", "r") as f:  # JSON
+    pw = json.load(f)
+    f.close()
+username = pw[0]["Spotify_User"]
+clientID = pw[0]["Spotify_ClientID"]
+clientSecret = pw[0]["Spotify_ClientSecret"]
+redirect_uri = 'http://localhost:8080/callback'
+
+oauth_object = spotipy.SpotifyOAuth(clientID, clientSecret, redirect_uri)
+token_dict = oauth_object.get_cached_token()
+token = token_dict['access_token']
+sp = spotipy.Spotify(auth=token)
+user_name = sp.current_user()
+
+def spotify_search(prompt):
+    res = sp.search(prompt, 1, 0, "track")
+    tracks = res["tracks"]["items"]
+    song = tracks[0]['external_urls']['spotify']
+    webbrowser.open(song)
+    return
+
+
